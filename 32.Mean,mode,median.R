dcdbe60779434841c095b1bfa507d537032072fb
praneetharoyal <141470150+praneetharoyal@users.noreply.github.com>
2024-03-19 23:09:46 +0530

    Add files via upload

---
+++ 32.Mean,mode,median.R
@@ -0,0 +1,7 @@
+values <- c(90,50,70,80,70,60,20,30,80,90,20)
+mean_value <- mean(values)
+cat("Mean:", mean_value, "\n")
+median_value <- median(values)
+cat("Median:", median_value, "\n")
+mode_value <- as.numeric(names(sort(table(values), decreasing = TRUE)[1]))
+cat("Mode:", mode_value, "\n")
---
+++ 33.Summary.R
@@ -0,0 +1,8 @@
+# create a vector with 10 elements
+data = c(1: 5, 56, 43, 56, 78, 51)
+
+# display
+print(data)
+
+# get summary
+print(summary(data))
